3552a28a29869055fd47da87d9d493865f9ba54f
ananovokmet <ana.novokmet@hotmail.com>
2021-07-10 19:52:07 +0200

    php fix 2

--- php.php
+++ php.php
@@ -15,6 +15,6 @@ $headers = "From: $email_from \r\n";
 $headers .="Reply-To: $email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
-header("location: hvala.html");
+header("location:hvala.html");
 
 ?>
